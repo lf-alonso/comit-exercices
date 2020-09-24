4dcffaa31b0231b4fc3f7bf3602861493f648cdc
lf-alonso <lfgalo@outlook.com>
2020-09-23 20:28:59 -0500

    Lesson 21

---
+++ Exercices/JS04/index.js
@@ -0,0 +1,5 @@
+"use strict"; 
+alert("Enter two numbers to see if the first is greater than the second");
+let fristNumber = prompt("Insert frist number:");
+let secondNumber = prompt("Insert second number:");
+result = confirm(fristNumber == secondNumber);
